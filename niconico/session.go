77856c2ac8a2bbe17c1208219ccf77c3ea5d38d8
Yasuaki Uechi <uetchy@randompaper.co>
2016-06-15 15:29:48 +0900

    Fix test

--- niconico/session.go
+++ niconico/session.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"errors"
 )
 
 func GetSessionKey(mail string, password string) (error, string) {
@@ -12,17 +13,20 @@ func GetSessionKey(mail string, password string) (error, string) {
 		Jar: jar,
 	}
 
+	if mail == "" || password == "" {
+		return errors.New("No email or password given"), ""
+	}
+
 	res, err := client.PostForm("https://secure.nicovideo.jp/secure/login?site=niconico",
 		url.Values{
 			"mail":     {mail},
 			"password": {password},
 		})
+	defer res.Body.Close()
 
 	if err != nil {
 		return err, ""
 	}
-
-	defer res.Body.Close()
 
 	u, _ := url.Parse("http://nicovideo.jp")
 	sessionKey := jar.Cookies(u)[1].String()
--- niconico/session_test.go
+++ niconico/session_test.go
@@ -9,7 +9,7 @@ import (
 func TestGetSessionKey(t *testing.T) {
 	email := os.Getenv("TEST_EMAIL")
 	pass := os.Getenv("TEST_PASS")
-	v := GetSessionKey(email, pass)
+	_, v := GetSessionKey(email, pass)
 	if !strings.HasPrefix(v, "user_session=user_session_") {
 		t.Error("Expected has prefix 'user_session=user_session_', got", v)
 	}
