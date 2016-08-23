c2485d0ef1e6621b1b2473b10610dc8c5c0971ef
Yasuaki Uechi <uetchy@randompaper.co>
2016-08-23 11:00:22 +0900

    test(session): handle error

--- niconico/session_test.go
+++ niconico/session_test.go
@@ -9,7 +9,10 @@ import (
 func TestGetSessionKey(t *testing.T) {
 	email := os.Getenv("TEST_EMAIL")
 	pass := os.Getenv("TEST_PASS")
-	_, v := GetSessionKey(email, pass)
+	err, v := GetSessionKey(email, pass)
+	if (err != nil) {
+		t.Error("Got error", err)
+	}
 	if !strings.HasPrefix(v, "user_session=user_session_") {
 		t.Error("Expected has prefix 'user_session=user_session_', got", v)
 	}
